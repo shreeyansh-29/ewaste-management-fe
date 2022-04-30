e880af87909100a6687aef897aeef356ba7c24f4
shreeyansh29 <shreeyansh.singh@nineleaps.com>
2022-04-30 20:45:28 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {CookiesProvider} from "react-cookie";
-
-ReactDOM.render(
-  <CookiesProvider>
-    <App />
-  </CookiesProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
