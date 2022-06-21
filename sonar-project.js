b2542bbe5297290d82c8d9140ef2bb5f06757f32
shreeyansh29 <shreeyansh.singh@nineleaps.com>
2022-06-21 10:38:47 +0530

    SonarQube Intregrated

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner(
     serverUrl: "http://localhost:9000",
     options: {
       "sonar.login": "admin",
-      "sonar.password": "Thala@3107",
+      "sonar.password": "Admin",
       "sonar.sources": ".",
       "sonar.inclusions": "src/**", // Entry point of your code
     },
